54206d099fd921ba970b46af80eb83d636a2d784
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-01-25 18:01:30 +0900

    feat: remove authjs authentication related packages and files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 //import { CreateBillForm } from "@/components/forms/create-bill-form";
 //import { CreateBillForm } from "@/components/create-bill-form";
-import { SignIn } from "@/components/sign-in";
-
+import LoginPage from "@/app/login/page";
 export default function Home() {
-  return <SignIn />;
+  return <LoginPage />;
 }
